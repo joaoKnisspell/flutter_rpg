5d0284fa6b33c53e6d08624989359baab73b205d
João <joaokz@outlook.com>
2025-04-01 19:16:17 -0300

    Custom Theme Initial Setup.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:rpg_app/screens/home/home.dart';
+import 'package:rpg_app/theme.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Home(),
+  runApp(MaterialApp(
+    theme: primaryTheme,
+    home: const Home(),
   ));
 }
